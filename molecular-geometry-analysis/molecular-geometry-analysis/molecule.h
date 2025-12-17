f508f73c38f96f1aea54fe1b88737e73d982ca5e
Ethan Song <ethan@dhcp-vl2041-45024.eduroam.cornell.edu>
2025-12-17 11:19:19 -0500

    odds) int=ultrafine guess=(read,mix) Project 1: flatten vectors

--- molecular-geometry-analysis/molecular-geometry-analysis/molecule.h
+++ molecular-geometry-analysis/molecular-geometry-analysis/molecule.h
@@ -12,6 +12,7 @@
 #include <cassert>
 #include <cmath>
 #include <cstdio>
+#include <vector>
 
 using namespace std;
 
@@ -21,35 +22,40 @@ public:
     int natoms;
     int charge;
     int multiplicity;
-    int *zvals;
-    double **coords;
+    vector<int> zvals;
+    vector<double> coords;
     string point_group;
-    double **distance_matrix;
+    vector<double> distance_matrix;
+    vector<double> angle_matrix;
     void print_geom();
     void compute_distances();
     void print_distance_matrix();
+    void compute_angles();
     Molecule(const char* filename, int q, int s);
-    ~Molecule();
+private:
+    inline int idx(int i, int j) const noexcept {
+        return i * natoms + j;
+    }
+    inline int idx3(int i, int j, int k) const noexcept {
+        return (i * natoms + j) * natoms + k;
+    }
 };
 
 void Molecule::print_geom() {
     cout << "Geometry\nNumber of atoms: " << natoms << endl;
     for (int i = 0; i < natoms; i++) {
-        printf("%d %6.3f %6.3f %6.3f\n", zvals[i], coords[i][0], coords[i][1], coords[i][2]);
+        printf("%d %6.3f %6.3f %6.3f\n", zvals[i], coords[idx(i,0)], coords[idx(i,1)], coords[idx(i,2)]);
     }
 }
 
 void Molecule::compute_distances() {
-    distance_matrix = new double*[natoms];
+    distance_matrix.resize(natoms * natoms);
     for (int i = 0; i < natoms; i++) {
-        distance_matrix[i] = new double[natoms];
-    }
-    for (int i = 0; i < natoms; i++) {
-        distance_matrix[i][i] = 0.;
+        distance_matrix[idx(i,i)] = 0.;
         for (int j = i + 1; j < natoms; j++) {
-            double dist = sqrt(pow(coords[i][0] - coords[j][0], 2) + pow(coords[i][1] - coords[j][1], 2) + pow(coords[i][2] - coords[j][2], 2));
-            distance_matrix[i][j] = dist;
-            distance_matrix[j][i] = dist;
+            double dist = sqrt(pow(coords[idx(i,0)] - coords[idx(j,0)], 2) + pow(coords[idx(i,1)] - coords[idx(j,1)], 2) + pow(coords[idx(i,2)] - coords[idx(j,2)], 2));
+            distance_matrix[idx(i,j)] = dist;
+            distance_matrix[idx(j,i)] = dist;
         }
     }
 }
@@ -58,10 +64,14 @@ void Molecule::print_distance_matrix() {
     cout << "Distance matrix\n";
     for (int i = 0; i < natoms; i++) {
         for (int j = 0; j < natoms; j++) {
-            printf("%.3f ", distance_matrix[i][j]);
+            printf("%.3f ", distance_matrix[idx(i,j)]);
         }
         cout << endl;
     }
+}
+
+void Molecule::compute_angles() {
+    angle_matrix.resize(pow(natoms,3));
 }
 
 Molecule::Molecule(const char* filename, int q, int s) {
@@ -70,22 +80,10 @@ Molecule::Molecule(const char* filename, int q, int s) {
     ifstream input(filename);
     assert(input.good());
     input >> natoms;
-    zvals = new int[natoms];
-    coords = new double*[natoms];
+    zvals.resize(natoms);
+    coords.resize(natoms * natoms);
     for (int i = 0; i < natoms; i++) {
-        coords[i] = new double[3];
-    }
-    for (int i = 0; i < natoms; i++) {
-        input >> zvals[i] >> coords[i][0] >> coords[i][1] >> coords[i][2];
+        input >> zvals[i] >> coords[idx(i,0)] >> coords[idx(i,1)] >> coords[idx(i,2)];
     }
     input.close();
-}
-
-Molecule::~Molecule() {
-    delete[] zvals;
-    for (int i = 0; i < natoms; i++) {
-        delete[] coords[i];
-        delete[] distance_matrix[i];
-    }
-    delete[] coords; delete[] distance_matrix;
 }
